64fe1282ffeb8732f3459f052bdfb1a420e8c3d1
Marcus Thelin <marcus.thelin@24hr.se>
2021-01-15 11:04:04 +0100

    Rest argument and an optinal options function that returns the Sentry instance

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 const Sentry = require('@sentry/node');
+const logger = require('./lib/logger');
+const packageName = require('./package.json').name;
 
-function init({ dsn, serviceName, release, environment = 'dev' }) {
+function init({ dsn, serviceName, release, environment = 'dev', ...rest }, optionFn) {
     Sentry.init({
         dsn,
         ...(release && { release }),
         environment,
+        beforeSend: (event, hint) => {
+            // Convert all non errors to error objects
+            const error = hint.originalException;
+            if (!(error instanceof Error)) {
+                return new Error(error);
+            }
+            return event;
+        },
+        ...rest,
     });
     Sentry.setTag('service', serviceName);
+    if (typeof optionFn === 'function') {
+        optionFn(Sentry);
+    }
+    logger.info(`From ${packageName}:`, 'Sentry has been initialized!');
 }
 
 module.exports = {
